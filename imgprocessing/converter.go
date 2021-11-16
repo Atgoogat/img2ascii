68402aad85216479ebc61286bbd009593d1c0649
Atgoogat <36623640+Atgoogat@users.noreply.github.com>
2021-11-16 20:45:34 +0100

    chore: speedup in conversion through use of an byte-buffer

--- imgprocessing/converter.go
+++ imgprocessing/converter.go
@@ -1,6 +1,7 @@
 package imgprocessing
 
 import (
+	"bytes"
 	"fmt"
 	"image"
 	"math"
@@ -27,17 +28,19 @@ func Convert(img image.Gray, symbols []string) (*string, error) {
 	dx, dy := bounds.Dx(), bounds.Dy()
 
 	symbolRelPos := float64(len(symbols) - 1)
-	out := ""
+	outBuffer := bytes.NewBuffer([]byte{})
+	outBuffer.Grow(dx * dy)
 	for y := 0; y < dy; y++ {
 		for x := 0; x < dx; x++ {
 			grayColor := img.GrayAt(x, y)
 			grayScale := float64(grayColor.Y) / 255
 
 			symbolIndex := int(math.Round(grayScale * symbolRelPos))
-			out += symbols[symbolIndex]
+			outBuffer.WriteString(symbols[symbolIndex])
 		}
-		out += fmt.Sprintln()
+		outBuffer.WriteString(fmt.Sprintln())
 	}
 
+	out := outBuffer.String()
 	return &out, nil
 }
